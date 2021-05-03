5cac8c17a7020a73ff0424ede99bd7bd8c75d514
khaleddsaadd <74107324+khaleddsaadd@users.noreply.github.com>
2021-05-04 00:18:57 +0200

    Images folder

---
+++ View/indexH.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<style>
+    body, html 
+    {
+        height: 100%;
+        margin: 0;
+    }
+    .indexBG
+    {
+        background-image: url("images/indexStart.jpg");
+        height: 100%; 
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .body1 
+    {
+        position: absolute;
+        width: 1291px;
+        height: 500px;
+        left: 115px;
+        top: 800px;
+        border: 5px solid rgba(192, 192, 192, 0.3);
+        box-shadow: 5px 5px rgb(224,224,224);
+    }
+    .body1Vid 
+    {
+        position: absolute;
+        top: 80px;
+        left: 20px;
+    }
+    .body1Text 
+    {
+        position: absolute;
+        width:300px;
+        height:300px;
+        top: 80px;
+        left: 800px;
+    }
+    .body2
+    {
+        position: absolute;
+        width: 1291px;
+        height: 500px;
+        left: 115px;
+        top: 1450px;
+    }
+    .body2_1
+    {
+        position: absolute;
+        width: 300px;
+        height: 350px;
+        border: 5px solid rgba(192, 192, 192, 0.3);
+        box-shadow: 5px 5px rgb(224,224,224);   
+    }
+    .body2_2
+    {
+        position: absolute;
+        width: 300px;
+        height: 350px;
+        left:500px;
+        border: 5px solid rgba(192, 192, 192, 0.3);
+        box-shadow: 5px 5px rgb(224,224,224);  
+    }
+    .body2_3
+    {
+        position: absolute;
+        width: 300px;
+        height: 350px;
+        right:0px;
+        border: 5px solid rgba(192, 192, 192, 0.3);
+        box-shadow: 5px 5px rgb(224,224,224);  
+    }
+    .body2_titles
+    {
+        font-family: 'Hind';
+        font-size: 18px;
+        font-weight: bold;
+    }
+    .body2_content
+    {
+        font-family: 'Hind';
+        font-size: 15px;
+        color: #CCA203
+    }
+    .Footer 
+    {
+        position: absolute;
+        width:100%;
+        height: 40%; 
+        top:280%;
+        background-color : #1F3351;
+
+    }
+</style>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="images/index.css">
+    </head>
+    <body>
+        <div class="indexBG">
+        </div>
+
+
+        <div class="body1">
+            <div class="body1Vid">
+                <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"></iframe>
+            </div>
+            <div class="body1Text">
+                <h1>Demo Text</h1>
+            </div>
+        </div>
+
+
+        <div class="body2">
+            <div class="body2_1">
+                <center><label class="body2_titles">Our mission</label></center>
+                <br><br>
+                <label class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                   officia deserunt mollit anim id est laborum. <label>
+            </div>
+            <div class="body2_2">
+            <center><label class="body2_titles">Our Plan</label></center>
+                <br><br>
+                <label class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                   officia deserunt mollit anim id est laborum. <label>
+            </div>
+            <div class="body2_3">
+            <center><label class="body2_titles">Our vision</label></center>
+                 <br><br>
+                 <label class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                   officia deserunt mollit anim id est laborum. <label>
+            </div>
+        </div>
+
+
+        <div class="Footer">
+            <img src=""
+        </div>
+        
+
+    </body>
+</html>
