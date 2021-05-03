e00b8c66a03a8f07e1f613296abf1629ae421a3d
reemahmedd9 <74379045+reemahmedd9@users.noreply.github.com>
2021-05-03 21:18:39 +0200

    RideH.php

---
+++ View/RideH.php
@@ -0,0 +1,24 @@
+!DOCTYPE html
+<html>
+<head>
+	<link rel="stylesheet" href="Ride.css">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+  <div class="col-md" id="rec1">
+
+<p>hi</p>
+
+
+  </div>
+  <div class="col-md">col-sm</div>
+  <div class="col-md">col-sm</div>
+</div>
+
+</div>
+
+
+</body>
+
+</html>
