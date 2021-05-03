419cdcff815c53be1ff77cf6378783860d09b8a9
khaleddsaadd <74107324+khaleddsaadd@users.noreply.github.com>
2021-05-03 21:17:08 +0200

    admin Reservation (HTML + CSS)

---
+++ View/admin_ReservationsH.php
@@ -0,0 +1,20 @@
+
+<html>  
+    <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="admin_Reservations.css">
+
+    </head>
+    <body>
+    <center>
+        <div id="resDiv"> 
+            <h1>Reservations List</h1>
+            <div id="resData">
+            </div>
+            <button id= "printBtn">Print reservations List</button> 
+           
+</script>
+        </div>
+    </center>
+    </body>
+</html>
