5d3d5351d38607173c29b589966b07000c5aa772
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-04 03:34:24 +0200

    Admin_Login

---
+++ View/admin_LoginH.php
@@ -0,0 +1,43 @@
+<html>  
+    <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="admin_Login.css">
+ 
+
+    </head>
+    <body>
+        <center>
+
+            <img src="NTLogo.png">
+            <h1>Admin Login</h1>
+        
+        </center>
+
+        <center>
+            <div class="frame">    
+        <form id="frame">   
+            <br><br><br><br><br>
+           
+            <label><b>Username
+            </b>
+            <br>
+            </label>    
+            <input type="text"  id="Username" placeholder="Username" name="Username" required="">    
+            <br><br>
+            <label><b>Password     
+            </b> 
+            <br>  
+            </label>    
+            <input type="Password" id="Password" placeholder="Password" name="Password" required="">    
+            <br><br><br><br>  
+
+            <input type="submit" name="loginbutton" id="loginbutton" value="Log in">       
+            <br><br>        
+
+        </form>
+            </div> 
+
+
+        </center>
+    </body>
+</html>
