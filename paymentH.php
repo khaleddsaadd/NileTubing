1593395941da622303b4b9b267bd496d9306e1cd
alyyasser7 <59626340+alyyasser7@users.noreply.github.com>
2021-05-04 04:32:01 +0200

    payment page

---
+++ paymentH.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<link rel="stylesheet" href="payment.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- <div id="body">
+    </div> -->
+    <div class="body2">
+        <img src="menu icon.png" width="50" alt="">
+    </div>
+<div class="content-box"> 
+    <h1 class="info">Fill Your Info To Checkout</h1>
+    <p class="Fname">First Name</p>
+    <input type="text" class="input Fnamerect">
+    <p class="Lname">Last Name</p>
+    <input type="text"class="input Lnamerect">
+    <p class="Mobileno">Mobile number</p>
+    <input type="text"class="input Mobilerect">
+    <p class="Email">Email address</p>
+    <input type="text"class="Emailrect">
+    <p class="Gender">Gender</p>
+    <select name="" class="input Genderrect">
+        <option value="">Male</option>
+        <option value="">Female</option>
+    </select>
+    <div class="payment-methods flex">
+        <p>Payment Methods:</p>
+        <input type="radio" name="payment"> <img src="cash.jpg" alt=""> Cash Payment
+        <input type="radio" name="payment"> <img src="fawry.jpg" alt=""> <img src="vf cash.jpeg" alt=""> <img src="etisalat.jpg" alt=""> Fawry/Vodafone Cash/Etisalat
+    </div>
+</div>
+
+
+<div class="content-box edited">
+    <div class="payment-methods edited flex">
+         <img src="fawry.jpg" alt=""> <img src="vf cash.jpeg" alt=""> <img src="etisalat.jpg" alt="">
+    </div>
+    <p class="Mobileno-edited">Mobile number</p>
+    <input type="text"class="Mobilerect-edited">
+    <p class="Email-edited">Email address</p>
+    <input type="text"class="Emailrect-edited">
+    <button class="rect4">Print PDF</button>
+</div>
+
+</body>
+
+</html>
