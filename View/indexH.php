fbf3d0a97b21a71f4f463dd9466199bdcd4ebe16
khaleddsaadd <74107324+khaleddsaadd@users.noreply.github.com>
2021-05-06 14:21:52 +0200

    Index + navbar

---
+++ View/indexH.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>     
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Bungee Outline' rel='stylesheet'>
+
+    </head>
+    <body>
+
+<div class="container">
+    <div class="row">
+        <div class="row-md">
+
+        <div class="indexBG">
+            <?php include 'userNavBar.html';?>
+            <img src="images/NTLogo.png" class="NTLogo_Header">
+            <label class="introText">GO WITH THE FLOW <br> NILE RIVER RUN</label>
+        </div>
+
+        </div>
+
+  <div class="row-md">
+        <div class="body1">
+            <div class="body1Vid">
+                <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"></iframe>
+            </div>
+            <div class="body1Text">
+                <h1>Demo Text</h1>
+            </div>
+        </div>
+  </div>
+
+
+<div class="row-md" id="body2">
+        <div class="body2">
+            <div class="col-md-4" id="body2_1">
+                <center><p class="body2_titles">Our mission</p>
+                <br><br></center>
+                <p class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+
+            <div class="col-md-4" id="body2_2">
+            <center><p class="body2_titles">Our Plan</p>
+                <br><br></center>
+                <p class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+
+            <div class="col-md-4" id="body2_3">
+            <center><p class="body2_titles">Our vision</p>
+                 <br><br></center>
+                 <p class="body2_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                 nisi ut aliquip ex ea commodo consequat. </p>
+           
+        </div>
+    </div>
+ </div>
+
+
+<!-- <div class="row" id="f"> -->
+<div class="row-md" id="Footer">
+            <div>
+                 <img src="images/NTLogo.png" id="NTLogo">
+            </div>
+
+            <div class="footer_Div_Left">
+                <p class="footer_Text_Left">Phone: +2001112705325 </p>
+                <br><br>
+                <p class="footer_Text_Left">Email: Niletubing@gmail.com</p>
+            </div> 
+            <img src="images/Loc.png" id="locImg">
+            <div class="footer_Div_Right">
+                <label class="footer_Text_Right">Egypt <br> Cairo Drops: <br> Dokki <br> Maadi <br> Zamalek <br> Moneeb </label>
+            </div>
+</div>
+
+        <div class="row-md" id="Footer2">
+            <div class="socialmedia">
+                <ul class="social">
+                <li>
+                    <a href="https://www.facebook.com/NileTubing" title="">
+                    <span class="icon fa fa-facebook " id="i"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/" title="">
+                    <span class="icon fa fa-instagram"  id="i"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://plus.google.com/" title="">
+                    <span class="icon fa fa-whatsapp"  id="i"></span>
+                    </a>
+                </li>
+                </ul>
+            </div>
+
+        </div>
+        </div>
+        
+
+       
+</div>
+<!-- </div> -->
+
+
+    </body>
+</html>
