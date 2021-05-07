085b736fe434eb2d22d3e0186f4296e4f298272b
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-08 01:12:30 +0200

    CU

---
+++ View/ContactUsH.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us</title>
+	<link rel="stylesheet" href="Contactus.css">
+    
+</head>
+
+
+<body>
+    
+<div class="container">
+	<div class="row-md" id="row1">
+        <h1 id="CU">Contact Us </h1>
+       <div id="nav"> <?php include 'userNavBar.html';?> </div>
+	</div>
+
+
+
+    <div class="row-md" id="box"></div>
+
+ 
+
+
+            <form>   
+                <br><br><br><br><br>
+               
+                <label><b>Name
+                </b>
+                <br>
+                </label>    
+                <input type="text"  id="text1"  name="name" required="">    
+                <br><br>
+       
+    
+            </form>
+    </div> 
+
+
+</div>
+
+
+
+
+</body>
+
+
+
+
+</html>
