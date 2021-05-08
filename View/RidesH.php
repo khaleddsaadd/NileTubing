b02093976c974d80ffdc190dc526c28c69e67693
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-08 14:09:54 +0200

    rides

---
+++ View/RidesH.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Rides</title>
+	<link rel="stylesheet" href="Rides.css">
+    
+</head>
+
+
+<body>
+    
+<div class="container">
+<div class="row">
+
+        <div class="row-md">
+            <div class="indexBG"></div>
+                <?php include 'userNavBar.html';?>     
+        </div>
+
+        <div class="row-md" id="box"> </div>
+
+        <div class="row-md" id="ST"> Single Tubing Rides</div>
+        
+    
+
+
+
+</div>
+</div>
