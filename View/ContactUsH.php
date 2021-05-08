5fce1f91f85cb69f5e2d961d866cfc6ff5979c92
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-08 05:18:24 +0200

    final CU

--- View/ContactUsH.php
+++ View/ContactUsH.php
@@ -10,18 +10,19 @@
 <body>
     
 <div class="container">
-	<div class="row-md" id="row1">
+<div class="row">
+        <div class="row-md">
+        
+        <div class="indexBG">
+         <?php include 'userNavBar.html';?>
         <h1 id="CU">Contact Us </h1>
-       <div id="nav"> <?php include 'userNavBar.html';?> </div>
-	</div>
+	    </div>
+        
+        </div>  
 
 
 
-    <div class="row-md" id="box"></div>
-
- 
-
-
+    <div class="row-md" id="box"> 
             <form>   
                 <br><br><br><br><br>
                
@@ -30,21 +31,52 @@
                 <br>
                 </label>    
                 <input type="text"  id="text1"  name="name" required="">    
-                <br><br>
-       
-    
+
+                <br><br><br><br><br><br><br><br><br><br>
+
+                <label><b>Email
+                </b>
+                <br>
+                </label>
+                <input type="text"  id="text2"  name="name" required="">    
+                <br><br><br><br><br><br><br><br><br><br>
+
+                <label><b>Gender
+                </b>
+                <br>
+                </label>
+                <select id="gender">
+                    <option> Male</option>
+                    <option> Female</option>
+                </select> 
+                <br><br><br><br><br><br><br><br><br><br>   
+
+                <label><b>Age
+                </b>
+                <br>
+                </label>
+                <input type="text"  id="text3"  name="name" required=""> 
+                <br><br><br><br><br><br><br><br><br><br>
+
+                <label><b>Write your message here
+                </b>
+                <br>
+                </label>
+                <input type="text"  id="text4"  name="name" > 
+                <br><br><br><br><br><br><br><br>
+
+                <input type="submit" name="send" id="send" value="Send">    
+
             </form>
+    </div>
     </div> 
+    
 
 
+</div>
 </div>
 
 
 
-
 </body>
-
-
-
-
 </html>
