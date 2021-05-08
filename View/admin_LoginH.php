34dbe4ad545c6e01007861465e935deead24d59d
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-08 22:00:30 +0200

    editing

--- View/admin_LoginH.php
+++ View/admin_LoginH.php
@@ -2,6 +2,10 @@
     <head>
          <meta name="viewport" content="width=device-width, initial-scale=1">
          <link rel="stylesheet" href="admin_Login.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Bungee Outline' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet'>
+
  
 
     </head>
