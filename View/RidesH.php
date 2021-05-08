8b65befed5809c8c559f9d0b743d6b18268bea88
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-05-08 20:52:35 +0200

    rides

--- View/RidesH.php
+++ View/RidesH.php
@@ -1,29 +1,56 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title> Rides</title>
-	<link rel="stylesheet" href="Rides.css">
+<html>     
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="Rides.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Bungee Outline' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet'>
+
     
-</head>
-
-
-<body>
-    
-<div class="container">
-<div class="row">
-
-        <div class="row-md">
-            <div class="indexBG"></div>
-                <?php include 'userNavBar.html';?>     
+    </head>
+    <body>
+        <div class="container">
+        <div class="row">
+                <div class="row-md">
+                        
+                    <div class="indexBG">
+                        <?php include 'userNavBar.html';?>
+                        <h1 class="indexText">Nile Tubing rides</h1>
+                    </div>
+                </div>
         </div>
+                <div class="row-md">
+                     <div class="body2">
+                        <label class="TripTitle">Single Tubing </label> 
+                        <h1>-->Trip image from database<--</h1>
+                    </div>
+                </div>
+                <div class="row-md">
+                     <div class="body3">
+                        <label class="TripTitle">Couple Tubing  </label> 
+                        <h1>-->Trip image from database<--</h1>
+                    </div>
+                </div>
+                <div class="row-md">
+                     <div class="body4">
+                        <label class="TripTitle">Group Tubing </label> 
+                        <h1>-->Trip image from database<--</h1>
+                    </div>
+                </div>
+                <div class="row-md">
+                    <div class="body5">
+                        <br><br>
+                        <label class="OfferEventText">Special Offers </label>
+                    </div>
+                </div>
+                <div class="row-md">
+                    <div class="body6">
+                        <br><br>
+                        <label class="OfferEventText">Upcoming Events </label>
+                    </div>
+                </div>
 
-        <div class="row-md" id="box"> </div>
-
-        <div class="row-md" id="ST"> Single Tubing Rides</div>
-        
-    
-
-
-
-</div>
-</div>
+        </div>
+    </body>
+</html>       
