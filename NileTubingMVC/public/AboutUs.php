d517642f7148f960831cd5650ea170697e13e529
reemahmedd9 <74379045+reemahmedd9@users.noreply.github.com>
2021-06-12 14:20:14 +0200

    About Us page

---
+++ NileTubingMVC/public/AboutUs.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+	<title>About Us</title>
+
+	<link rel="stylesheet" href="AboutUs.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<include src="./View/userNavBar.html"></include>
+</head>
+
+
+<body>
+	<?php include 'userNavBar.html';?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="row-md" id="row1">
+
+			<h1 id="AU">About Us </h1>
+
+		</div>
+
+    <div class="row-md" id="rec2">
+	
+<div class="row-md"> 
+<p id="p1"> We are a group of Young Egyptian Doctors who decided to entertain and bring joy and relaxation to the famous Nile river.</p>
+</div> 
+
+
+<div class="row">
+
+	<div class="col-md-4">
+		<img  id="L" src="./Images/NTLogo.png">
+	</div>
+	
+
+
+<div class="col-md-8" >
+	<p id="p2">Simply by enjoying the view and exploring Egypt in the process. All you have to do is just to sit down and float your way.</p>
+
+</div>
+
+</div>
+
+
+ 
+    </div>
+
+<div class="row" id="rec3">
+	
+<div class="col-md" id="body1Vid">
+	
+
+<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"></iframe>
+
+</div>
+
+<div class="col-md" id="par">
+	
+
+<p>Offers service for all age group over 16. <br> Zero efforts. <br> Cheapest price.</p>
+
+
+</div>
+
+
+
+
+</div>
+
+<div class="row" id="rec4">
+	
+<h1 id="Lear">Learn More</h1>
+
+
+
+
+
+</div>
+
+
+	</div>
+
+
+
+</div>
+
+
+
+
+</body>
+
+
+
+
+</html>
