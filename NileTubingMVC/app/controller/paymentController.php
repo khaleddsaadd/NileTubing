cef1e69db242ce2e32c846508e9aa0d87ec70fc5
khaleddsaadd <74107324+khaleddsaadd@users.noreply.github.com>
2021-06-15 14:45:44 +0200

    Payment Part

--- NileTubingMVC/app/controller/paymentController.php
+++ NileTubingMVC/app/controller/paymentController.php
@@ -1,7 +1,53 @@
+
+<script>
+    function createPDF() {
+        var sTable = document.getElementById('prr').innerHTML;
+
+        var style = "<style>";
+        style = style + "table {width: 100%;font: 17px Calibri;}";
+        style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+        style = style + "padding: 2px 3px;text-align: center;}";
+        style = style + "#title{padding-left:27%; font-size:30px; color:red; font-weight:bolder;}";
+        style = style + "label{ font-size:20px; color:#1F3351; font-weight:bold;}";
+        style = style + ".first{ font-size:20px; color:black; font-weight:bolder;}";
+        style = style + ".tprice{ font-size:30px; padding-left:30%; color:red; font-weight:bolder;}";
+
+
+
+
+        style = style + "</style>";
+
+        var win = window.open('', '', 'height=700,width=700');
+
+        win.document.write('<html><head>');
+        win.document.write('<title>Reservations List</title>');   
+        win.document.write(style);         
+        win.document.write('</head>');
+        win.document.write('<body>');
+        win.document.write(sTable);     
+        win.document.write('</body></html>');
+
+        win.document.close(); 	
+
+        win.print();    
+    }
+</script> 
 <?php 
 require_once(__ROOT__ . "controller/Controller.php");
 class paymentController extends Controller
 {
-    
+    function insert()
+    {
+        $FName = $_REQUEST["FName"];
+        $LName = $_REQUEST["LName"];
+        $mobile = $_REQUEST["Mobile"];
+        $PaymentMethod = $_REQUEST["payment"];
+        $mail = $_REQUEST["Mail"];
+        $RideID = $_GET['id'];
+        $Date=$_GET['date'];
+        $Quantity = $_GET['q'];
+        $this->model->SaveReservation($FName,$LName,$mobile,$PaymentMethod,$mail,$RideID,$Date,$Quantity);
+
+    }
 }
 ?>
--- NileTubingMVC/app/model/paymentModel.php
+++ NileTubingMVC/app/model/paymentModel.php
@@ -1,8 +1,112 @@
+
 <?php
 require_once(__ROOT__ . "model/Model.php");
 class payment extends Model
 {
     public $tittle = 'Payment';
+    public $id;
+    public $name;
+    public $price;
+    public $userFirstName;
+    public $userLastName;
+    public $userEmail;
+    public $userPhoneNumber;
+    public $userGender;
+    function __construct($id="")
+    {
+        $this->db = $this->connect();
+        if($id!="")
+        {
+            $sql = "SELECT * FROM rides where id='$id'";
+            $result = $this->db->query($sql);
+            if ($result->num_rows == 1)
+            {
+                $row = $this->db->fetchRow();
+                $this->id = $row["id"];
+                $this->name = $row["Name"];
+                $this->price = $row["Price"];
+        }
+    }
+    }
+    function readRide($type)
+    {
+        $sql = "SELECT * FROM rides where R_Type='$type'";
+		$result = $this->db->query($sql);
+		if ($result->num_rows == 1)
+        {
+            $row = $this->db->fetchRow();
+            $this->id = $row["id"];
+            $this->name = $row["Name"];
+            $this->price = $row["Price"];
+        }   
+    }
+    function get_id()
+    {
+        return $this->id;
+    }
+    function get_name()
+    {
+        return $this->name;
+    }
+ function get_price()
+    {
+        return $this->price;
+    }
+    function SaveReservation($FName,$LName,$mobile,$PaymentMethod,$mail,$RideID,$Date,$Quantity)
+    {
+         $Total = $this->price * $Quantity;
+         $rideName = $this->name;
+        $sql = "INSERT INTO `reservations`(`Name`, `Mobile`, `Email` , `PaymentMethod`, `ID_Ride`, `Date_Time`, `Quantity` , `TotalPrice`)
+         VALUES ('$FName','$mobile','$mail','$PaymentMethod','$RideID','$Date','$Quantity' , '$Total')";
+		if($this->db->query($sql) === true)
+        {
+            echo "<br><div id='alr'> <div class = 'alert alert-success'>
+            Thank You For Joining Nile Tubing!
+            </div></div> ";            echo "<div id='prr' style='display:none;'>
+                    <label id='Title'>$FName's Nile Tubing Ticket</label> <br>
+                    <br><br>
+                    <label>Contact Info</label>
+                    <hr>
+                    <label>Name: $FName $LName </label><br>
+                    <label>Mobile: $mobile </label><br>
+                    <label>E-Mail: $mail </label><br>
+                    <label>Gender:  </label><br>
 
+
+                    <br><br>
+                    <label>Trip Info</label>
+                    <hr>
+                    <label>Payment Method: $PaymentMethod </label><br>
+                    <label>Ride Name: $rideName </label><br>
+                    <label>Reservation Date: $Date </label><br>
+                    <label>Quantity: $Quantity Person/s </label><br>";
+                    if($PaymentMethod=="Cash")
+                    {
+                        echo '<hr>
+                        <label class="tprice">Total Price: '.$Total.' EGP</label>';
+                    }
+                    else
+                    {
+                      echo '<br><br>
+                      <label>Payment Details</label>
+                      <hr>
+                      <label>Fawry: </label><br>
+                      <label>Vodafone Cash:  </label><br>
+                      <label>Etisalat Cash: </label><br>';
+                      echo '<hr>
+                      <label class="tprice">Total Price: '.$Total.' EGP</label>';
+                    }
+
+                    
+
+               echo  "</div>";
+            echo '<script>
+            createPDF();
+        </script>';
+		} 
+		else
+        {
+		}
+    }
 }
 ?>
