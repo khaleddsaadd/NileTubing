e62f38e796f7971968e9f8d73c464558c0bc4a8f
alyyasser7 <59626340+alyyasser7@users.noreply.github.com>
2021-06-19 18:52:14 +0200

    update

--- NileTubingMVC/app/view/checkoutView.php
+++ NileTubingMVC/app/view/checkoutView.php
@@ -62,25 +62,6 @@ class checkouts extends View
                 </div>';
         $str.='';
         $str .= '
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Checkout Confirmation</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              Press confirm to continue to the paymnet method
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-              <a href="payment.php?id='.$this->model->get_id().'&date='.$this->model->get_date().'&q='.$this->model->get_quantity().'"> <button type="button" class="btn btn-primary">Confrim</button></a>
-            </div>
-          </div>
-        </div>
-      </div>
       <script>
 function myFunction() {
   var txt;
